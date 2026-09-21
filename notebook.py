29d1408e9113d5c08306c167b70472fe1600f8f9
coline m <coline.mouze@gmail.com>
2026-09-21 22:21:21 +0200

    Refactor Fibonacci function

--- notebook.py
+++ notebook.py
@@ -15,10 +15,8 @@ def _():
 
 @app.function
 def Fibonacci(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n < 2:
+        return n
     return Fibonacci(n - 1) + Fibonacci(n - 2)
 
 
