9f3c4a324d307b51c550c54c40cc044255727060
coline m <coline.mouze@gmail.com>
2026-09-21 22:07:33 +0200

    add fibonacci function signature

--- notebook.py
+++ notebook.py
@@ -9,24 +9,13 @@ def _():
     import marimo as mo
     import pytest as py
 
+
     return
 
 
 @app.function
-#def Fibonacci (n):
-#    Fibonacci=[0,1]
-#   for k in range (2,n+1):
-#        Fibo_k = Fibonacci[k-1] + Fibonacci[k-2]
-#        Fibonacci.append(Fibo_k_k)
-#    return F[n]
-
-def Fibonacci (n):
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
+def Fibonacci(n):
+    pass
 
 
 @app.function
@@ -38,8 +27,8 @@ def test_Fiboinit():
 
 @app.function
 def test_Fibo():
-    assert Fibonacci(2) == 1
-    assert Fibonacci(4) == 3
+    assert Fibonacci(3) == 1
+    assert Fibonacci(4) == 1
     assert Fibonacci(12) == 144
     return
 
